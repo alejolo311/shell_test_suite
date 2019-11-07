1c22258e5eef6a7e6fba9fe1a65f4846a1481169
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-07 18:16:52 +0000

    Git user name as an alternative al shell line

--- checker.sh
+++ checker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#
+# Usage: checker [git_user_name]
+#
 
+# Veify if the username was sent
 if [ "$1" == "" ]
 then
 	echo "write your Github username"
@@ -8,7 +12,7 @@ else
 	USER=$1
 fi
 
-#find . -name "simple_shell" -exec rm -rf {} \;
+# Delete the previous project directory if exists
 [ -d simple_shell ] && rm -rf simple_shell
 
 git clone "https://github.com/$USER/simple_shell.git"
@@ -16,11 +20,12 @@ git clone "https://github.com/$USER/simple_shell.git"
 cd simple_shell
 
 #verify if gcc is the version 4.8.4
-
 gcc -Wall -Werror -Wextra -pedantic *.c -o hsh
 if [ $? == 1 ]
 then
 	echo "Compilation Error!"
 fi
 
+# Final message
+echo
 echo "Final revision successful!"
