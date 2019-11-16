9958dca0dacd720d49b036cf4852e470c05f7fa4
Alejo López <1147@holbertonschool.com>
2019-11-16 11:53:01 -0500

    change the way how is verify the contain of the file

--- task_3_checks.sh
+++ task_3_checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\n########## Task 3  ##########\n"
+echo -e "\n########## Task 3  #########\n"
 
 # cases
 echo "/bin/ls" | /bin/sh > ../a
@@ -8,31 +8,25 @@ echo "/bin/ls" | ./hsh > ../b
 diff ../a ../b > ../logs/logs_0
 rm ../a
 rm ../b
-export RES=$(find .. -name logs_0 -empty -printf "0")
-if [ a$RES == a0 ]
+if [ -s ../logs/logs_0 ]
 then
+	../fail_check.sh
+else
 	rm -rf ../logs/logs_0
 	../success_check.sh
-else
-	../fail_check.sh
 fi
 
 echo "./hsh: 1: /bin/l: not found" > ../a
-echo "/bin/l" | ./hsh  > ../b
+echo "/bin/l" | ./hsh > ../b
 diff ../a ../b > ../logs/logs_1
 rm ../a
 rm ../b
-export RES=$(find .. -name logs_1 -empty -printf "0")
-if [ a$RES == a0 ]
+if [ -s ../logs/logs_1 ]
 then
+	../fail_check.sh
+else
 	rm -rf ../logs/logs_1
 	../success_check.sh
-else
-	../fail_check.sh
 fi
-
-
-
-
 
 echo -e "\n######## End task 3 ########\n"
