e91ec7fa971ab0e5ad1cb3d613c6a1911191ca8c
alejolo311 <1147@holbertonschool.com>
2019-11-15 21:55:57 -0500

    sepate in a new file the print of the check and add colors to the print red to fail and green to succes

---
+++ fail_check.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m' 
+
+echo -e "${RED}###### 0 - Check Fail ######${NC}\n"
---
+++ success_check.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m' 
+
+echo -e "${GREEN}#### 0 - Check Success #####${NC}\n"
