53881291d2d1823489d3715a21f2eb7417da61e2
alejolo311 <1147@holbertonschool.com>
2019-11-15 22:32:39 -0500

    adding a new test case

--- task_0_checks.sh
+++ task_0_checks.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-echo -e "\nTask 0\n"
-#ls
-echo -e "case 0: /bin/ls\n"
+echo -e "\n########## Task 0 ##########\n"
+
+# cases
 echo "/bin/ls" | /bin/sh > ../a
 echo "/bin/ls" | ./hsh > ../b
-diff ../a ../b > ../logs_0
+diff ../a ../b > ../logs/logs_0
 rm ../a
 rm ../b
 export RES=$(find .. -name logs_0 -empty -printf "0")
 if [ a$RES == a0 ]
 then
-	rm ../logs_0
-	echo -e "0 - Check Success\n"
+	rm -rf ../logs/logs_0
+	../success_check.sh
 else
-	echo -e "0 - Check Fail - check the logs file for the explanation\n"
+	../fail_check.sh
 fi
 
+echo "./hsh: 1: /bin/l: not found" > ../a
+echo "/bin/l" | ./hsh  > ../b
+diff ../a ../b > ../logs/logs_1
+rm ../a
+rm ../b
+export RES=$(find .. -name logs_1 -empty -printf "0")
+if [ a$RES == a0 ]
+then
+	rm -rf ../logs/logs_1
+	../success_check.sh
+else    
+	../fail_check.sh
+fi
+
+
+
+
+
+echo -e "\n######## End task 0 ########\n"
