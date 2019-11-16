b64f0d80e752228f8d04b58542b6ddb681086177
Alejo López <1147@holbertonschool.com>
2019-11-16 12:40:33 -0500

    fix the pathe of the betty files

--- task_1_checks.sh
+++ task_1_checks.sh
@@ -6,7 +6,7 @@ echo -e "\n########## Task 1 ##########\n"
 
 betty-style *.c *.h > ../logs/betty-style.txt
 
-if [ -s betty-style.txt ]
+if [ -s ../logs/betty-style.txt ]
 then
 	../fail_check.sh
 else
@@ -16,7 +16,7 @@ fi
 
 betty-doc *.c *.h 2> ../logs/betty-doc.txt 1>../logs/funcs.txt
 
-if [ -s betty-doc.txt ]
+if [ -s ../logs/betty-doc.txt ]
 then
 	../fail_check.sh
 else
