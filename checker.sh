e3af722265bbb19e4a17bd69e521b9d0ed370a73
alejolo311 <1147@holbertonschool.com>
2019-11-15 21:36:18 -0500

    adding comments to explain ina better way the function of th script and adding the log folder creation

--- checker.sh
+++ checker.sh
@@ -13,23 +13,41 @@ else
 fi
 
 # Delete the previous project directory if exists
+
 [ -d simple_shell ] && rm -rf simple_shell
+
+# clone the repo
 
 git clone "https://github.com/$USER/simple_shell.git"
 
+#change to the repo folder
+
 cd simple_shell
 
-#verify if gcc is the version 4.8.4
+# Compile with all the flags
+
 gcc -Wall -Werror -Wextra -pedantic *.c -o hsh
+
+# check if the compile finish in success
+
 if [ $? == 1 ]
 then
 	echo "Compilation Error!"
 	exit -1
 fi
 
-# Final message
+# Create a log folder to store the error logs
 
+[ -d ../logs ] && rm -rf ../logs
+mkdir ../logs
+
+# Call the sh file that contains the text cases
 
 ../task_0_checks.sh
 
+# Delete the folder that was clone
+cd ..
+[ -d simple_shell ] && rm -rf simple_shell
+
+# finish the script.
 echo "Final revision successful!"
