b4cfa7eb38aa8def118ef46730c5b9ebd2d2a78c
Alejo López <1147@holbertonschool.com>
2019-11-17 19:26:44 +0000

    adding the check for the task 2

--- tasks/task_2_checks.sh
+++ tasks/task_2_checks.sh
@@ -3,5 +3,14 @@
 echo -e "\n########## Task 2 ##########\n"
 
 # cases
-../results/success_check.sh
+GITUSER=`git config user.name`
+AUTHORS="../AUTHORS"
+grep -r "$GITUSER" $AUTHORS > ../isaauthor
 
+if [ -s ../isaauthor ]
+then
+        ../results/success_check.sh
+else
+    	../results/fail_check.sh
+fi
+[ -f ../isaauthor ] && rm ../isaauthor
