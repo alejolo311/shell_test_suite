7b533d75745ca18dffc78732c908b3e677c2bb10
Alejo López <1147@holbertonschool.com>
2019-11-18 15:54:18 +0000

    add two new cases

--- tasks/task_3_checks.sh
+++ tasks/task_3_checks.sh
@@ -28,3 +28,35 @@ else
 	rm -rf ../logs/logs_1
 	../results/success_check.sh
 fi
+
+echo "./././hsh: 1: /bin/l: not found" > ../a
+echo "/bin/l" | ./././hsh 2> ../b
+diff ../a ../b > ../logs/logs_2
+rm ../a
+rm ../b
+if [ -s ../logs/logs_2 ]
+then
+	../results/fail_check.sh
+else
+	rm -rf ../logs/logs_2
+	../results/success_check.sh
+
+fi
+
+touch ../test_ls_2
+echo -e "/bin/ls\n/bin/ls" > ../test_ls_2
+
+cat ../test_ls_2 | /bin/sh > ../a
+cat ../test_ls_2 | ./hsh > ../b
+diff ../a ../b > ../logs/logs_3
+rm ../a
+rm ../b
+if [ -s ../logs/logs_3 ]
+then
+	../results/fail_check.sh
+else
+	rm -rf ../logs/logs_3
+	rm -rf ../test_ls_2
+	../results/success_check.sh
+
+fi
